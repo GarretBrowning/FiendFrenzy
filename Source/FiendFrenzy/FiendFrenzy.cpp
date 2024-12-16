77f864b553eaca6a7b20d6f728ff89244ead629a
Garret Browning <garret.browning777@gmail.com>
2024-12-15 19:46:18 -0600

    Adding first initial project files to the repo

---
+++ Source/FiendFrenzy/FiendFrenzy.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FiendFrenzy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FiendFrenzy, "FiendFrenzy" );
+ 
---
+++ Source/FiendFrenzy/FiendFrenzyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FiendFrenzyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFiendFrenzyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFiendFrenzyGameMode();
+};
+
+
+
